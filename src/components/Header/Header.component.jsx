0064eba5790361696b1f88fbc16bbb43226e5197
udaya28 <54870625+udaya28@users.noreply.github.com>
2020-12-28 13:18:07 +0530

    completed logout

--- src/components/Header/Header.component.jsx
+++ src/components/Header/Header.component.jsx
@@ -1,10 +1,33 @@
 import { AppBar, Grid, IconButton, Toolbar } from '@material-ui/core';
-import { React } from 'react';
+import { React, useState, useContext } from 'react';
 import MenuIcon from '@material-ui/icons/Menu';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import Button from '@material-ui/core/Button';
-import './Header.styles.css'
+import './Header.styles.css';
+import AlertDialog from './../Dialog/Dialog.component';
+import { setLogin } from '../../context/context';
+import cookies from 'js-cookie';
+
 const Header = () => {
+  const setIsLoggedIn = useContext(setLogin);
+  const signOut = () => {
+    handleOpenDialog();
+  };
+  const doneSignOut = () => {
+    cookies.remove('token');
+    setIsLoggedIn(false);
+  };
+  //dialog
+  const [OpenDialog, setOpenDialog] = useState(false);
+
+  const handleOpenDialog = () => {
+    setOpenDialog(true);
+  };
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+  };
+
   return (
     <AppBar position="fixed" color="primary">
       <Toolbar>
@@ -24,15 +47,21 @@ const Header = () => {
           </Grid>
           <Grid item>
             <Grid container direction="row" alignItems="center">
-              <Button endIcon={<ExitToAppIcon className="white" />}>
-                <h4 className="mon white">
-                  Sign Out
-                </h4>
+              <Button endIcon={<ExitToAppIcon className="white" />} onClick={signOut}>
+                <h5 className="mon white">Logout</h5>
               </Button>
             </Grid>
           </Grid>
         </Grid>
       </Toolbar>
+      <AlertDialog
+        SetOpen={OpenDialog}
+        handleClose={handleCloseDialog}
+        title="Confirm"
+        content="Are you sure you want to logout?"
+        handleConfirm={doneSignOut}
+        confirmButtonColorSecondary={true}
+      />
     </AppBar>
   );
 };

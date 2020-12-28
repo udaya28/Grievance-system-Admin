db5e94134b0de6cd5b20189aeab2c5754d5a1596
udaya28 <54870625+udaya28@users.noreply.github.com>
2020-12-28 19:56:58 +0530

    added footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+const Footer = () => {
+  return (
+    <AppBar
+      position="relative"
+      style={{ top: 'auto', bottom: 0 }}
+      color="primary"
+    >
+      <Grid container justify="space-between" style={{ padding: '10px 5px' }}>
+        <Grid item sm={6} xs={12}>
+          <Typography variant="caption">Copyright © 2020</Typography>
+        </Grid>
+        <Grid item sm={6} xs={12}>
+          <Typography variant="caption">
+            Developed By Udaya with ❤️
+          </Typography>
+          {/* Anonymous */}
+        </Grid>
+      </Grid>
+    </AppBar>
+  );
+};
+
+export default Footer;
--- src/components/home/Home.component.jsx
+++ src/components/home/Home.component.jsx
@@ -1,10 +1,13 @@
 import { Grid} from '@material-ui/core';
 import { React } from 'react';
 import Header from '../Header/Header.component';
+import Footer from '../Footer/Footer'
 const Home = () => {
   return (
     <Grid container direction="column">
       <Header/>
+      <div style={{height:'100vh'}}>dsfhkj</div>
+      <Footer/>
     </Grid>
   );
 };

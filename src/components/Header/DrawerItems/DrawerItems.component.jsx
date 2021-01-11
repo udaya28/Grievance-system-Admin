d66bbe7bee3a6d7988c1c21700673be5f69b479b
udaya28 <54870625+udaya28@users.noreply.github.com>
2021-01-11 16:47:07 +0530

    completed route

--- src/components/Header/DrawerItems/DrawerItems.component.jsx
+++ src/components/Header/DrawerItems/DrawerItems.component.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
@@ -19,26 +20,32 @@ const DrawerItems = () => {
   return (
     <div style={{ width: '250px' }}>
       <List>
-        <ListItem
-          button
-          selected={selectedIndex === 0}
-          onClick={() => setSelectedIndex(0)}
-        >
-          <ListItemIcon>
-            <MoveToInboxIcon />
-          </ListItemIcon>
-          <ListItemText primary="Complaints" />
-        </ListItem>
-        <ListItem
-          button
-          selected={selectedIndex === 1}
-          onClick={() => setSelectedIndex(1)}
-        >
-          <ListItemIcon>
-            <AssessmentIcon />
-          </ListItemIcon>
-          <ListItemText primary="Dashboard" />
-        </ListItem>
+        <Link to="/Grievance-system-Admin/complaints">
+          <ListItem
+            button
+            selected={selectedIndex === 0}
+            onClick={() => setSelectedIndex(0)}
+          >
+            <ListItemIcon>
+              <MoveToInboxIcon />
+            </ListItemIcon>
+            <ListItemText primary="Complaints" />
+          </ListItem>
+        </Link>
+
+        <Link to="/Grievance-system-Admin/dashboard">
+          <ListItem
+            button
+            selected={selectedIndex === 1}
+            onClick={() => setSelectedIndex(1)}
+          >
+            <ListItemIcon>
+              <AssessmentIcon />
+            </ListItemIcon>
+            <ListItemText primary="Dashboard" />
+          </ListItem>
+        </Link>
+
         <ListItem
           button
           selected={selectedIndex === 2}
@@ -55,48 +62,57 @@ const DrawerItems = () => {
         </ListItem>
         <Collapse in={openDropDown} timeout="auto" unmountOnExit>
           <List component="div" disablePadding style={{ paddingLeft: '32px' }}>
-            <ListItem
-              button
-              selected={selectedIndex === 3}
-              onClick={() => setSelectedIndex(3)}
-            >
-              <ListItemIcon>
-                <GroupAddIcon />
-              </ListItemIcon>
-              <ListItemText primary="Create" />
-            </ListItem>
-            <ListItem
-              button
-              selected={selectedIndex === 4}
-              onClick={() => setSelectedIndex(4)}
-            >
-              <ListItemIcon>
-                <UpdateIcon />
-              </ListItemIcon>
-              <ListItemText primary="Update" />
-            </ListItem>
-            <ListItem
-              button
-              selected={selectedIndex === 5}
-              onClick={() => setSelectedIndex(5)}
-            >
-              <ListItemIcon>
-                <DeleteForeverIcon />
-              </ListItemIcon>
-              <ListItemText primary="Delete" />
-            </ListItem>
+            <Link to="/Grievance-system-Admin/create-student">
+              <ListItem
+                button
+                selected={selectedIndex === 3}
+                onClick={() => setSelectedIndex(3)}
+              >
+                <ListItemIcon>
+                  <GroupAddIcon />
+                </ListItemIcon>
+                <ListItemText primary="Create" />
+              </ListItem>
+            </Link>
+            <Link to="/Grievance-system-Admin/update-student">
+              <ListItem
+                button
+                selected={selectedIndex === 4}
+                onClick={() => setSelectedIndex(4)}
+              >
+                <ListItemIcon>
+                  <UpdateIcon />
+                </ListItemIcon>
+                <ListItemText primary="Update" />
+              </ListItem>
+            </Link>
+
+            <Link to="/Grievance-system-Admin/delete-student">
+              <ListItem
+                button
+                selected={selectedIndex === 5}
+                onClick={() => setSelectedIndex(5)}
+              >
+                <ListItemIcon>
+                  <DeleteForeverIcon />
+                </ListItemIcon>
+                <ListItemText primary="Delete" />
+              </ListItem>
+            </Link>
           </List>
         </Collapse>
-        <ListItem
-          button
-          selected={selectedIndex === 6}
-          onClick={() => setSelectedIndex(6)}
-        >
-          <ListItemIcon>
-            <AccountBoxIcon />
-          </ListItemIcon>
-          <ListItemText primary="Profile" />
-        </ListItem>
+        <Link to="/Grievance-system-Admin/profile">
+          <ListItem
+            button
+            selected={selectedIndex === 6}
+            onClick={() => setSelectedIndex(6)}
+          >
+            <ListItemIcon>
+              <AccountBoxIcon />
+            </ListItemIcon>
+            <ListItemText primary="Profile" />
+          </ListItem>
+        </Link>
       </List>
     </div>
   );

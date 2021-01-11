a2f7ba8206887bf1403b693d0f378addfa9ea353
udaya28 <54870625+udaya28@users.noreply.github.com>
2021-01-11 16:32:09 +0530

    added route successfully

---
+++ src/components/Student/UpdateStudent/UpdateStudent.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UpdateStudent = () => {
+    return (
+        <div>
+            <h1>Update</h1>
+        </div>
+    )
+}
+
+export default UpdateStudent
--- src/components/home/Home.component.jsx
+++ src/components/home/Home.component.jsx
@@ -1,14 +1,52 @@
 import { Grid } from '@material-ui/core';
 import { React } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from 'react-router-dom';
 import Header from '../Header/Header.component';
 import Footer from '../Footer/Footer';
+import Complaints from '../Complaints/Complaints.component';
+import Dashboard from '../Dashboard/Dashboard.component';
+import Profile from '../Profile/Profile.component';
+import CreateStudent from '../Student/CreateStudent/CreateStudent.component';
+import UpdateStudent from '../Student/UpdateStudent/UpdateStudent.component';
+import DeleteStudent from '../Student/DeleteStudent/DeleteStudent.component';
 const Home = () => {
   return (
     <Router>
       <Grid container direction="column">
         <Header />
-        <div style={{ height: '100vh' }}>dsfhkj</div>
+        <div style={{ height: '50vh' }}></div>
+        <Switch>
+          <Route exact path="/Grievance-system-Admin/create-student">
+            <CreateStudent />
+          </Route>
+          <Route exact path="/Grievance-system-Admin/update-student">
+            <UpdateStudent />
+          </Route>
+          <Route exact path="/Grievance-system-Admin/delete-student">
+            <DeleteStudent />
+          </Route>
+          <Route exact path="/Grievance-system-Admin/profile">
+            <Profile />
+          </Route>
+          <Route exact path="/Grievance-system-Admin/dashboard">
+            <Dashboard />
+          </Route>
+          <Redirect
+            exact
+            from="/Grievance-system-Admin/"
+            to="/Grievance-system-Admin/complaints"
+          />
+          <Route exact path="/Grievance-system-Admin/complaints">
+            <Complaints />
+          </Route>
+        </Switch>
+        <div style={{ height: '50vh' }}></div>
         <Footer />
       </Grid>
     </Router>

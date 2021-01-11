5fd4c677de93db23a3685a3b0af997ba250c1a10
udaya28 <54870625+udaya28@users.noreply.github.com>
2021-01-11 15:46:59 +0530

    changed title

--- src/components/home/Home.component.jsx
+++ src/components/home/Home.component.jsx
@@ -1,14 +1,17 @@
-import { Grid} from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import { React } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Header from '../Header/Header.component';
-import Footer from '../Footer/Footer'
+import Footer from '../Footer/Footer';
 const Home = () => {
   return (
-    <Grid container direction="column">
-      <Header/>
-      <div style={{height:'100vh'}}>dsfhkj</div>
-      <Footer/>
-    </Grid>
+    <Router>
+      <Grid container direction="column">
+        <Header />
+        <div style={{ height: '100vh' }}>dsfhkj</div>
+        <Footer />
+      </Grid>
+    </Router>
   );
 };
 

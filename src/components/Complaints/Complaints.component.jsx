7df8804633b582c84acc851837e809344630d616
udaya28 <54870625+udaya28@users.noreply.github.com>
2021-01-20 20:03:53 +0530

    fixess

--- src/components/Complaints/Complaints.component.jsx
+++ src/components/Complaints/Complaints.component.jsx
@@ -1,18 +1,20 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import ComplaintBar from './ComplaintBar/ComplaintBar.component';
 import FilterDrawer from './Drawer/Drawer.component';
 import Feed from './Feed/Feed.component';
 import './Complaints.styles.css';
 import Grid from '@material-ui/core/Grid';
-
+import Container from '@material-ui/core/Container';
 const Complaints = () => {
-  const [openFilter, setOpenFilter] = useState(false)
+  const [openFilter, setOpenFilter] = useState(false);
   return (
     <Grid container className="complaint-container">
       <Grid item className="feed-container">
         <Grid container direction="column">
           <ComplaintBar setOpenFilter={setOpenFilter} />
-          <Feed />
+          <Container maxWidth="md">
+            <Feed />
+          </Container>
         </Grid>
       </Grid>
       <Grid item>

a593ef07feca68d88d26b8d3f88484d928437e4c
udaya28 <54870625+udaya28@users.noreply.github.com>
2021-01-27 15:27:37 +0530

    added controlled componnt

--- src/components/Student/CreateStudent/CreateStudent.component.jsx
+++ src/components/Student/CreateStudent/CreateStudent.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import FormControl from '@material-ui/core/FormControl';
 import FormGroup from '@material-ui/core/FormGroup';
@@ -10,9 +10,27 @@ import Typography from '@material-ui/core/Typography';
 import './createStudent.styles.css';
 import Container from '@material-ui/core/Container';
 const CreateStudent = () => {
+  const [studentData, setStudentData] = useState({
+    firstName: '',
+    secondName: '',
+    departmentName: '',
+    jointYear: '',
+    gender: '',
+    rollNumber: '',
+    dateOfBirth: '',
+    password: '',
+  });
+  const handleInput = (e) => {
+    setStudentData({...studentData,[e.target.name]:e.target.value})
+  };
+  const handleFormSubmit = () =>{
+    console.log(studentData)
+  }
   return (
     <Container maxWidth="md">
-      <h1 style={{textAlign:"left",margin:"10px 0px 0px"}}>Create Student</h1>
+      <h1 style={{ textAlign: 'left', margin: '10px 0px 0px' }}>
+        Create Student
+      </h1>
       <FormGroup className="form-group1">
         <Grid container>
           <Grid items xs={12} sm={6}>
@@ -21,8 +39,9 @@ const CreateStudent = () => {
                 id="outlined-basic"
                 label="First Name"
                 variant="outlined"
-                // value={title}
-                // onInput={handleTitle}
+                name="firstName"
+                value={studentData.firstName}
+                onInput={handleInput}
                 // error={ValidationState && title === ''}
               />
             </FormControl>
@@ -33,8 +52,9 @@ const CreateStudent = () => {
                 id="outlined-basic"
                 label="Second Name"
                 variant="outlined"
-                // value={title}
-                // onInput={handleTitle}
+                name="secondName"
+                value={studentData.secondName}
+                onInput={handleInput}
                 // error={ValidationState && title === ''}
               />
             </FormControl>
@@ -46,8 +66,9 @@ const CreateStudent = () => {
                 id="outlined-basic"
                 label="Roll Number"
                 variant="outlined"
-                // value={title}
-                // onInput={handleTitle}
+                name="rollNumber"
+                value={studentData.rollNumber}
+                onInput={handleInput}
                 // error={ValidationState && title === ''}
               />
             </FormControl>
@@ -58,8 +79,9 @@ const CreateStudent = () => {
                 id="outlined-basic"
                 label="Password"
                 variant="outlined"
-                // value={title}
-                // onInput={handleTitle}
+                name="password"
+                value={studentData.password}
+                onInput={handleInput}
                 // error={ValidationState && title === ''}
               />
             </FormControl>
@@ -71,8 +93,9 @@ const CreateStudent = () => {
                 id="outlined-basic"
                 label="Department Name"
                 variant="outlined"
-                // value={title}
-                // onInput={handleTitle}
+                name="departmentName"
+                value={studentData.departmentName}
+                onInput={handleInput}
                 // error={ValidationState && title === ''}
               />
             </FormControl>
@@ -83,8 +106,9 @@ const CreateStudent = () => {
                 id="outlined-basic"
                 label="Joint Year"
                 variant="outlined"
-                // value={title}
-                // onInput={handleTitle}
+                name="jointYear"
+                value={studentData.jointYear}
+                onInput={handleInput}
                 // error={ValidationState && title === ''}
               />
             </FormControl>
@@ -96,8 +120,9 @@ const CreateStudent = () => {
                 id="outlined-basic"
                 label="Gender"
                 variant="outlined"
-                // value={title}
-                // onInput={handleTitle}
+                name="gender"
+                value={studentData.gender}
+                onInput={handleInput}
                 // error={ValidationState && title === ''}
               />
             </FormControl>
@@ -107,24 +132,25 @@ const CreateStudent = () => {
               <TextField
                 id="outlined-basic"
                 label="Date of Birth"
-                helperText="Date in DD-MM-YYYY"
+                helperText="DD-MM-YYYY"
                 variant="outlined"
-                // value={title}
-                // onInput={handleTitle}
+                name="dateOfBirth"
+                value={studentData.dateOfBirth}
+                onInput={handleInput}
                 // error={ValidationState && title === ''}
               />
             </FormControl>
           </Grid>
           <Grid container justify="flex-end">
-          <Button
-            variant="contained"
-            color="primary"
-            endIcon={<SendIcon />}
-            // onClick={handleFormSubmit}
-          >
-            Create
-          </Button>
-        </Grid>
+            <Button
+              variant="contained"
+              color="primary"
+              endIcon={<SendIcon />}
+              onClick={handleFormSubmit}
+            >
+              Create
+            </Button>
+          </Grid>
         </Grid>
       </FormGroup>
     </Container>

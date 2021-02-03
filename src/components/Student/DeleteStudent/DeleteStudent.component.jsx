4641891c7d55ae77d97597b0da33346943d7093a
udaya28 <54870625+udaya28@users.noreply.github.com>
2021-02-03 08:19:24 +0530

    added serch bar to students list

--- src/components/Student/DeleteStudent/DeleteStudent.component.jsx
+++ src/components/Student/DeleteStudent/DeleteStudent.component.jsx
@@ -11,5 +11,4 @@ const DeleteStudent = () => {
     </Container>
   );
 };
-
 export default DeleteStudent;
--- src/components/Student/StudentList/StudentList.component.jsx
+++ src/components/Student/StudentList/StudentList.component.jsx
@@ -10,17 +10,63 @@ import AppBar from '@material-ui/core/AppBar';
 import IconButton from '@material-ui/core/IconButton';
 import Grid from '@material-ui/core/Grid';
 import Toolbar from '@material-ui/core/Toolbar';
+import InputBase from '@material-ui/core/InputBase';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search';
 const StudentList = ({ buttonText, buttonColor }) => {
   const studentDetails = useContext(studentDetailsContext);
   const refreshStudentDetails = useContext(refreshStudentDetailsContext);
   console.log(studentDetails);
+
+  const useStyles = makeStyles((theme) => ({
+    search: {
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: fade(theme.palette.common.white, 0.15),
+      '&:hover': {
+        backgroundColor: fade(theme.palette.common.white, 0.25),
+      },
+      marginLeft: 0,
+      width: '100%',
+    },
+    searchIcon: {
+      padding: theme.spacing(0, 2),
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    inputRoot: {
+      color: 'inherit',
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 0),
+      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        width: '12ch',
+    },
+  }));
+  const classes = useStyles();
   return (
     <div container style={{ margin: '15px 0px' }}>
-      <AppBar position="sticky" className="student-details-bar">
+      <AppBar position="sticky" className="student-details-bar" size="">
         <Toolbar>
           <Grid container justify="space-between" alignItems="center">
-            <Grid item>
-              <p>Div</p>
+            <Grid item >
+              <div className={classes.search}>
+                <div className={classes.searchIcon}>
+                  <SearchIcon />
+                </div>
+                <InputBase
+                  placeholder="Search…"
+                  classes={{
+                    root: classes.inputRoot,
+                    input: classes.inputInput,
+                  }}
+                  inputProps={{ 'aria-label': 'search' }}
+                />
+              </div>
             </Grid>
             <Grid item>
               <IconButton onClick={() => refreshStudentDetails()}>
